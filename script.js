590e91b05af49bdbe35b7b30aeeaa0c716089048
aedgell2007-dev <aedgell2007@gmail.com>
2025-10-08 13:58:12 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const showSecretBtn = document.getElementById('showSecret');
-  const hiddenLink = document.querySelector('.hidden-link');
-  showSecretBtn.addEventListener('click', () => {
-    hiddenLink.style.display = 'inline';
-    showSecretBtn.style.display = 'none';
-  });
-});
